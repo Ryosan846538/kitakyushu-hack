7c4c54e43f6061b89cfc29487a29d5981ccf36a9
Ryota Asai <ryota846538@outlook.jp>
2024-03-23 02:07:19 +0900

    コンポーネント化と微修正

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { InputField } from "@/components/InputField";
+import { TextareaField } from "@/components/TextareaField";
 
 function FormPage() {
   return (
@@ -12,52 +14,31 @@ function FormPage() {
         <form className="mt-8 space-y-6" action="#" method="POST">
           <input type="hidden" name="remember" value="true" />
           <div className="-space-y-px rounded-md shadow-sm">
-            <input
-              id="company"
-              name="company"
-              type="text"
-              required
-              className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder:text-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-lg"
-              placeholder="あなたの会社"
-            />
+            <InputField id="company" name="company" placeholder="会社名" />
 
-            <input
-              id="week"
-              name="week"
-              type="text"
-              required
-              className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder:text-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-lg"
-              placeholder="希望日程"
-            />
+            <InputField id="week" name="week" placeholder="希望日程" />
 
-            <textarea
+            <TextareaField
               id="activity"
               name="activity"
-              required
-              className="relative block w-full resize-none appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder:text-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-lg"
               placeholder="何をするか"
-              rows={4}
+              rows={6}
             />
 
-            <textarea
+            <TextareaField
               id="request"
               name="request"
-              required
-              className="relative block w-full resize-none appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder:text-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-lg"
               placeholder="事前の要望"
-              rows={4}
+              rows={6}
             />
 
-            <input
+            <InputField
               id="email"
               name="email"
               type="email"
-              required
-              className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder:text-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-lg"
               placeholder="メールアドレス"
             />
           </div>
-
           <div>
             <button
               type="submit"
---
+++ src/components/TextareaField.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface TextareaFieldProps {
+  id: string;
+  name: string;
+  placeholder: string;
+  rows: number;
+  required: boolean;
+}
+
+export function TextareaField({
+  id,
+  name,
+  placeholder = "",
+  rows = 3,
+  required = false,
+}: TextareaFieldProps) {
+  return (
+    <textarea
+      id={id}
+      name={name}
+      rows={rows}
+      required={required}
+      className="relative block w-full resize-none appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder:text-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-lg"
+      placeholder={placeholder}
+    />
+  );
+}
+
+export default TextareaField;

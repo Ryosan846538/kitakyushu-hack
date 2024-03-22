1b5310fc5adbc7395c4c9178eb6c6c50e1760ce8
Ryota Asai <ryota846538@outlook.jp>
2024-03-23 01:11:26 +0900

    ヘッダーと申し込みフォームの作成

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -12,71 +12,50 @@ function FormPage() {
         <form className="mt-8 space-y-6" action="#" method="POST">
           <input type="hidden" name="remember" value="true" />
           <div className="-space-y-px rounded-md shadow-sm">
-            <div>
-              <label htmlFor="company" className="sr-only">
-                あなたの会社
-                <input
-                  id="company"
-                  name="company"
-                  type="text"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder:text-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-lg"
-                  placeholder="あなたの会社"
-                />
-              </label>
-            </div>
-            <div>
-              <label htmlFor="week" className="sr-only">
-                希望日程
-                <input
-                  id="week"
-                  name="week"
-                  type="text"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder:text-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-lg"
-                  placeholder="希望日程"
-                />
-              </label>
-            </div>
-            <div>
-              <label htmlFor="activity" className="sr-only">
-                何をするか
-                <textarea
-                  id="activity"
-                  name="activity"
-                  required
-                  className="relative block w-full resize-none appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder:text-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-lg"
-                  placeholder="何をするか"
-                  rows="4"
-                />
-              </label>
-            </div>
-            <div>
-              <label htmlFor="request" className="sr-only">
-                事前の要望
-                <textarea
-                  id="request"
-                  name="request"
-                  required
-                  className="relative block w-full resize-none appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder:text-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-lg"
-                  placeholder="事前の要望"
-                  rows="4"
-                />
-              </label>
-            </div>
-            <div>
-              <label htmlFor="email" className="sr-only">
-                メールアドレス
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder:text-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-lg"
-                  placeholder="メールアドレス"
-                />
-              </label>
-            </div>
+            <input
+              id="company"
+              name="company"
+              type="text"
+              required
+              className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder:text-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-lg"
+              placeholder="あなたの会社"
+            />
+
+            <input
+              id="week"
+              name="week"
+              type="text"
+              required
+              className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder:text-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-lg"
+              placeholder="希望日程"
+            />
+
+            <textarea
+              id="activity"
+              name="activity"
+              required
+              className="relative block w-full resize-none appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder:text-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-lg"
+              placeholder="何をするか"
+              rows={4}
+            />
+
+            <textarea
+              id="request"
+              name="request"
+              required
+              className="relative block w-full resize-none appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder:text-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-lg"
+              placeholder="事前の要望"
+              rows={4}
+            />
+
+            <input
+              id="email"
+              name="email"
+              type="email"
+              required
+              className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder:text-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-lg"
+              placeholder="メールアドレス"
+            />
           </div>
 
           <div>

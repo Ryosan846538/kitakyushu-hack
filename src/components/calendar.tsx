59036cb3c79247d4ebe110d52293b33c27bd9424
Ishigami100 <yamahaya5151@icloud.com>
2024-03-23 22:52:11 +0900

    カレンダー追加処理を入れる

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -3,12 +3,19 @@
 import jaLocale from "@fullcalendar/core/locales/ja";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import FullCalendar, { EventInput } from "@fullcalendar/react";
+import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import React, { useState } from "react";
 
 function CalendarComponent() {
-  const [events] = useState<EventInput[]>([
+  interface EventType {
+    title: string;
+    start: string;
+    end?: string;
+    allDay?: boolean;
+  }
+
+  const [events, setEvents] = useState<EventType[]>([
     { title: "eventを", start: "2024-03-14" },
     {
       title: "こんな感じで追加できます",
@@ -16,6 +23,26 @@ function CalendarComponent() {
       end: "2024-03-17",
     },
   ]);
+
+  const handleEventAdd = (info: { start?: Date; end?: Date }) => {
+    // 入力ダイアログ
+    const eventName = prompt("イベントを入力してください");
+
+    if (eventName) {
+      // イベントの追加
+      const newEvent: EventType = {
+        title: eventName,
+        start: info.start?.toISOString() ?? "", // FullCalendarのイベント型に合わせてプロパティを修正
+        end: info.end?.toISOString() ?? "", // FullCalendarのイベント型に合わせてプロパティを修正
+        allDay: true,
+      };
+      setEvents((prevEvents: EventType[]) => {
+        const updatedEvents: EventType[] = [...prevEvents]; // 新しい配列を作成し、既存の要素を展開
+        updatedEvents.unshift(newEvent); // 新しいイベントを配列の先頭に追加
+        return updatedEvents; // 新しい配列を返す
+      });
+    }
+  };
 
   return (
     <div>
@@ -31,7 +58,7 @@ function CalendarComponent() {
         }}
         events={events}
         selectable
-        // select={handleEventAdd}
+        select={handleEventAdd}
       />
     </div>
   );

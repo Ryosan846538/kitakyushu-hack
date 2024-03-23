da40fdad0febf8435fda54f53996dd80ab6f6e10
Shion Ichikawa <shion1305@gmail.com>
2024-03-24 08:56:01 +0900

    wip

--- Makefile
+++ Makefile
@@ -13,4 +13,4 @@ gen_proto_go:
 	protoc --proto_path=./proto/src --go_out=./proto/pb_go --go_opt=paths=source_relative --go-grpc_out=./proto/pb_go --go-grpc_opt=paths=source_relative ./proto/src/*.proto
 
 gen_proto_ts:
-	npx protoc --ts_out=./src/pb_ts --proto_path=./proto/src ./proto/src/*.proto
+	protoc --ts_proto_opt=nestJs=true --plugin=./node_modules/.bin/protoc-gen-ts_proto.ps1 --ts_proto_out=./src/pb_ts/ --proto_path=./proto/src ./proto/src/*.proto
--- src/components/ChatUi.tsx
+++ src/components/ChatUi.tsx
@@ -1,8 +1,23 @@
 "use client";
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { grpc } from "@improbable-eng/grpc-web";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 import ChatBot from "react-simple-chatbot";
+import { ChatRequest, ChatResponse } from "@/pb_ts/chat";
+import { OpenAIServiceClient } from "@/pb_ts/chat.client";
+
+function getResponse(messages: string[]): ChatResponse {
+  // define rpc transport
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-assignment
+  const transport = grpc.CrossBrowserHttpTransport({ withCredentials: true });
+
+  const client = new OpenAIServiceClient(transport);
+  const chatRequest = new ChatRequest();
+  chatRequest.setMessagesList(messages);
+  return chatRequest.toObject() as ChatResponse;
+}
 
 function ChatUi() {
   const steps = [

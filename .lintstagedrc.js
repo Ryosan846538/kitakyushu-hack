7219c5a0784da3441a179fc3c094919c66ddf33f
Shion Ichikawa <shion1305@gmail.com>
2024-03-24 08:13:46 +0900

    🔧 configure ignore for pb generated files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,27 @@
+const { ESLint } = require("eslint");
 const path = require("path");
 
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(" --file ")}`;
+const targetFiles = async (files) => {
+  const eslint = new ESLint();
+  const isIgnored = await Promise.all(
+    files.map((file) => {
+      return eslint.isPathIgnored(file);
+    }),
+  );
+  return files.filter((_, i) => !isIgnored[i]);
+};
 
 module.exports = {
-  "*.{js,jsx,ts,tsx}": [buildEslintCommand, "yarn format"],
+  "*.{js,jsx,ts,tsx}": [
+    (filenames) =>
+      `yarn lint:fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`,
+    async (filenames) => {
+      const files = await targetFiles(filenames);
+      if (files.length === 0) {
+        return "";
+      }
+      return `yarn lint:fix --file ${files.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`;
+    },
+    "yarn format",
+  ],
 };

605351cef7450a85187348e49ec5442783af73ae
Ryota Asai <ryota846538@outlook.jp>
2024-03-24 12:49:29 +0900

    submitの完成

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { Text, Button, Checkbox } from "@yamada-ui/react";
+import Link from "next/link";
 import React, { useState } from "react";
 import { InputField } from "@/components/InputField";
 import { TextareaField } from "@/components/TextareaField";
-// import { useRouter } from "next/router";
 
 interface FormData {
   lastName: string;
@@ -246,13 +246,15 @@ function FormPage() {
               </div>
             </div>
           </div>
-          <Button
-            type="submit"
-            className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            colorScheme="primary"
-          >
-            送信
-          </Button>
+          <Link href="/submit">
+            <Button
+              type="submit"
+              className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              colorScheme="primary"
+            >
+              送信
+            </Button>
+          </Link>
         </form>
       </div>
     </div>
